bc08ddb2167f2dc8d9677ad0e652b16e7da3e149
AwdiTech <raby.awdi@gmail.com>
2024-03-25 00:13:43 -0400

    Fixed catch all route to use regex instead of * due to not working properly with React SPA

--- src/index.js
+++ src/index.js
@@ -4,9 +4,8 @@ import gameRoutes from './api/routes/gameRoutes.js';
 import userRoutes from './api/routes/userRoutes.js';
 import { errorHandler } from './api/middlewares/errorMiddleware.js';
 import cors from 'cors';
-import path from 'path'; // Include path module
+import path from 'path';
 import { fileURLToPath } from 'url';
-// Adjust __dirname to point to the server's root directory instead of src
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 import * as cfg from './config/config.js';
 const PORT = cfg.PORT;
@@ -21,14 +20,14 @@ app.use(express.json());
 app.use('/api/game', gameRoutes);
 app.use('/api/user', userRoutes);
 
-// Adjusted to serve static files from the root's build folder
 app.use(express.static(path.join(__dirname, '..', 'build')));
 
-// The "catchall" handler, which sends back the built client app for any request that doesn't match a route
-app.get('*', (req, res) => {
+// The "catchall" handler, which sends back the built client app for any request that doesn't match a route.
+app.get(/^\/(?!api).*/, (req, res) => {
     const indexPath = path.join(__dirname, '..', 'build', 'index.html');
     res.sendFile(indexPath);
 });
+
 
 app.use(errorHandler);
 
